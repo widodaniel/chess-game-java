21628c2ee4de70a09a715bfeb1ac82eb9a1c7a3b
Daniel Silveira <danielsilveiraluna07@gmail.com>
2021-03-09 09:21:39 -0300

    Knight

--- src/application/UI.java
+++ src/application/UI.java
@@ -60,7 +60,7 @@ public class UI {
 		if(!chessMatch.getCheckMate()) {
 			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 			if (chessMatch.getCheck()) {
-				System.out.println();
+				System.out.println("CHECK!");
 			}
 		}
 		else {
